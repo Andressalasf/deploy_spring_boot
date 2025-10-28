a413aecb18ed360728d66b1e2705b5ef1b841e59
Andressalasf <humkyjimp@gmail.com>
2025-10-28 15:02:10 -0500

    fix

--- src/main/java/com/parcialspring/parcialspring/config/CorsConfig.java
+++ src/main/java/com/parcialspring/parcialspring/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000") // url React
+                .allowedOrigins("http://localhost:5173", "http://localhost:3000","https://deploy-granme.vercel.app" ) // url React
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") //Metodos http permitidos
                 .allowedHeaders("*")
                 .allowCredentials(true);
